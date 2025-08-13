0ef822a07724ecf8dd8a5c7f2eea1259d137ed48
Rawan Ahmed <RawanSadek267@gmail.com>
2025-08-13 18:39:29 +0300

    Fix case sensitivity for Navbar

---
+++ src/Modules/Shared/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import userAvatar from '../../../../assets/Images/userAvatar.png'
+
+export default function Navbar({ loginData }) {
+  // console.log(loginData)
+
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light navbar-expand-lg justify-content-between p-3 rounded-4">
+        <div className="container-fluid">
+          <a className="navbar-brand">Food App</a>
+
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#UserInfo" aria-controls="UserInfo" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id='UserInfo'>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+              <li className="nav-item me-4 d-flex">
+                <img src={userAvatar} alt="user img" className="rounded-circle avatar-img" />
+                <span className='me-5 nav-link'>{loginData?.userName}</span>
+              </li>
+              <li className="nav-item">
+                <button className="btn nav-link p-0 border-0 bg-transparent">
+                  <i className="fa-solid fa-bell"></i>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </nav>
+    </>
+  )
+}
---
+++ src/Modules/Shared/Components/SideBar/SideBar.jsx
@@ -0,0 +1,32 @@
+import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
+import logo from '../../../../assets/Images/titlelogo.png'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function SideBar({ logout }) {
+  let [collapse, setCollapse] = useState(false);
+  let getCollapse=()=>{
+    setCollapse(!collapse);
+  }
+
+  useEffect(() => {
+    const width = collapse ? '80px' : '250px';
+    document.documentElement.style.setProperty('--sidebar-w', width);
+  }, [collapse]);
+
+  return (
+    <div className="sidebar-container">
+      <Sidebar className="vh-100 position-fixed sidebar" collapsed={collapse}>
+        <img onClick={getCollapse} src={logo} alt="logo" className="w-50 mt-5 mx-3" />
+        <Menu className="mt-5">
+          <MenuItem component={<Link to='home'></Link>} icon={<i className="fa-solid fa-house"></i>}> Home </MenuItem>
+          <MenuItem component={<Link to='users'></Link>} icon={<i className="fa-solid fa-users"></i>}> Users </MenuItem>
+          <MenuItem component={<Link to='recipes'></Link>} icon={<i className="fa-regular fa-newspaper"></i>}> Recipes </MenuItem>
+          <MenuItem component={<Link to='categories'></Link>} icon={<i className="fa-regular fa-calendar-days"></i>}> Categories </MenuItem>
+          <MenuItem icon={<i className="fa-solid fa-unlock-keyhole"></i>}> Change Password </MenuItem>
+          <MenuItem onClick={logout} component={<Link to='/login'></Link>} icon={<i className="fa-solid fa-right-from-bracket"></i>}> Logout </MenuItem>
+        </Menu>
+      </Sidebar>
+    </div>
+  )
+}
