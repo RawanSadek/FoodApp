e2afebee97400046a1cb33ecf163ece97017f6b4
Rawan Ahmed <RawanSadek267@gmail.com>
2025-08-26 23:37:13 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // 👈 tells Vite to output the build here
+  }
 })
