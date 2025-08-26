b973491f544a65e555c704ccf98fb194a42f275e
Rawan Ahmed <RawanSadek267@gmail.com>
2025-08-26 23:24:41 +0300

    updated

--- src/Modules/Shared/Components/AuthLayout/AuthLayout.jsx
+++ src/Modules/Shared/Components/AuthLayout/AuthLayout.jsx
@@ -7,9 +7,9 @@ export default function AuthLayout() {
 
   const isRegister = location.pathname === "/register";
   return (
-    <div className='authContainer vh-100 position-relative'>
+    <div className='authContainer  position-relative'>
       <div className="container-fluid auth-overlay h-100">
-        <div className="row vh-100 justify-content-center align-items-center ">
+        <div className="row min-vh-100 justify-content-center align-items-center ">
           <div className={`${isRegister ? "col-md-8" : "col-md-5"} bg-white rounded-4 px-5 py-4`}>
             <div className="logo text-center">
               <img src={logo} alt="logo" className={`${isRegister? 'w-30':'w-75'}`} />
